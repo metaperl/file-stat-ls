0c7e14103109c95e6374262d0f84a713b7b723f0
Terrence Brannon <metaperl@gmail.com>
2011-06-02 14:27:12 -0400

    works on recursive dir structures

--- script/mojo/mojo.pl
+++ script/mojo/mojo.pl
@@ -7,8 +7,12 @@ get '/dirls' => sub {
 
     use File::Stat::Ls;
     my $physical_folder = $self->param('folder') ? $self->param('folder') : 'public/dl';
+    my $physical_folder_as_url = do {
+	my @part = split '/', $physical_folder;
+	"/" . join '/', @part[1..$#part];
+    };
     my $o = File::Stat::Ls->new(folder => $physical_folder);
-    $self->render('baz', ls => $o, controller_url => '/dirls', physical_folder_as_url => '/dl');
+    $self->render('baz', ls => $o, controller_url => '/dirls', physical_folder_as_url => $physical_folder_as_url);
 };
 
 app->secret('soap');
